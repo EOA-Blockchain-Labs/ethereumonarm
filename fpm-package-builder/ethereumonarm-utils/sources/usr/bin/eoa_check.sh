4e76572975f8bb7d3e38dd2092dce8cbeaadf9bc
Diego Losada <diego.losadaf@gmail.com>
2023-01-10 13:53:13 +0100

    eoa_check script improvements

--- fpm-package-builder/ethereumonarm-utils/sources/usr/bin/eoa_check.sh
+++ fpm-package-builder/ethereumonarm-utils/sources/usr/bin/eoa_check.sh
@@ -1,30 +1,43 @@
 #!/usr/bin/env bash
 
-# Check if the user is root
-if [ "$(id -u)" != "0" ]; then
-   # If the user is not root, print an error message and exit
-   echo -e "Error: \033[31mYou must be Root to run this script\033[0m. please run sudo eoa_check.sh" 
-   exit 1
-fi
+RED='\033[0;91m'
+GREEN='\033[0;92m'
+NC='\033[0m'
+DISKS=0
+NVME_DISK=false
+USB_DISK=false
 
-consensus_layer="geth erigon besu nethermind"
-execution_layer="prysm-beacon teku lighthouse-beacon nimbus"
+execution_layer="geth erigon besu nethermind"
+consensus_layer="prysm-beacon teku lighthouse-beacon nimbus"
 readonly consensus_layer execution_layer
 
 consensus_is_running=false
 execution_is_running=false
 
+# Check if the user is root
+if [ "$(id -u)" != "0" ]; then
+   # If the user is not root, print an error message and exit
+   echo -e "Error: ${RED}You must be Root to run this script${NC}. please run sudo eoa_check.sh" 
+   exit 1
+fi
+
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+
+# Check Ethereum user
 if id "ethereum" >/dev/null 2>&1
 then
-	echo "User ethereum exists"
+	echo -e "Ethereum user check ${GREEN}✓${NC} ethereum user exists"
 else
-	echo -e "\033[31mUser ethereum does not exist, installation failed\033[0m"
+	echo -e "${RED}User ethereum does not exist, installation failed${NC}. Please see /var/log for more info"
+fi
+
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 
 for c in ${consensus_layer} 
 do
 	if (systemctl is-active --quiet $c)
 	then
-		echo "$c service is running"
+		echo -e "Consensus Layer check ${GREEN}✓${NC} $c service is running"
 		consensus_is_running=true
 		continue		
 	fi
@@ -34,91 +47,91 @@ for e in ${execution_layer}
 do
 	if (systemctl is-active --quiet $e)
 	then
-		echo "$e service is running"
+		echo -e "Execution Layer check ${GREEN}✓${NC} $e service is running"
 		execution_is_running=true
 		continue		
 	fi
 done
 
-fi
-
 if [ "${consensus_is_running}" = false ]
 then
-	echo -e "\033[31mNo Consensus Client is running\033[0m"
+	echo -e "Error: ${RED}No Consensus Client is running${NC}. Please, start a Consensus Layer client"
 	exit 0
 fi
 
 if [ "${execution_is_running}" = false ]
 then
-	echo -e "\033[31mNo Execution Client is running\033[0m"
+	echo -e "${RED}No Execution Client is running${NC}. Please, start an Execution Layer client"
 	exit 0
 fi
 
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+
 # Check JWT file
 if test -s "/etc/ethereum/jwtsecret"; then
-    echo "The file /etc/ethereum/jwtsecret exists and is not empty"
+    echo -e "JWT secret file check ${GREEN}✓${NC} File exists and it is not empty"
 else
-    echo -e "\033[31mThe file /etc/ethereum/jwtsecret does not exist or it is empty\033[0m"
+    echo -e "${RED}The file /etc/ethereum/jwtsecret does not exist or it is empty${NC}"
 fi
+
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 
 # Check 8551 port
 if nc -z localhost 8551; then
-    echo "Port 8551 is listening and accepting connections"
+    echo -e "Port 8551 check ${GREEN}✓${NC} Port is listening and accepting connections"
 else
-    echo -e "\033[31mPort 8551 is not open. Execution Layer and Consensus Layer cannot communicate between\033[0m"
+    echo -e "${RED}Port 8551 is not open. Execution Layer and Consensus Layer cannot communicate between${NC}"
 fi
 
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo ""
-echo ""
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo ""
+# Check if device '/dev/sda' exists
+if [ -e "/dev/sda" ]; then
+  DISKS=$((DISKS+1))
+  USB_DISK=true
+fi
+
+# Check if device '/dev/nvme0n1' exists
+if [ -e "/dev/nvme0n1" ]; then
+  DISKS=$((DISKS+1))
+  NVME_DISK=true
+fi
+
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+
+if [ $DISKS -eq 0 ]; then
+  echo -e "${RED}Error: There are no SSD disks present or mounted${NC}. Please review your set up or /etc/fstab config."
+  exit 1
+else
+  echo -e "Disk check ${GREEN}✓${NC} External disk detected"
+fi
+
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 
 lsblk
 
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo ""
-echo ""
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo ""
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 
-if [ -e "/dev/sda" ]
+if [[ $USB_DISK == true ]]
 then
-	echo "The /dev/sda disk model is `smartctl -a /dev/sda | grep "Model Number" | awk '{ $1=$2="";$0=$0;} NF=NF'`"
+	echo -e "The USB SSD disk model is `smartctl -a /dev/sda | grep "Model Number" | awk '{ $1=$2="";$0=$0;} NF=NF'`"
 fi
-if [ -e "/dev/nvme0n1" ]
+if [[ $NVME_DISK == true ]]
 then
-	echo "The /dev/nvme0n1 disk model is `smartctl -a /dev/nvme0n1 | grep "Model Number" | awk '{ $1=$2="";$0=$0;} NF=NF'`"
+	echo -e "The NVME Disk model is `smartctl -a /dev/nvme0n1 | grep "Model Number" | awk '{ $1=$2="";$0=$0;} NF=NF'`"
 fi
 
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo ""
-echo ""
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo ""
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 
 top -bcn 1
 
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo ""
-echo ""
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo ""
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 
 ss -tunlp
 
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo ""
-echo ""
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo ""
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 
-curl ifconfig.co/port/30303
-curl ifconfig.co/port/9000
+curl ifconfig.co/port/30303 -w "\n"
+curl ifconfig.co/port/9000 -w "\n"
 
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-echo ""
-echo ""
-echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 
 tail -n 100 /var/log/syslog

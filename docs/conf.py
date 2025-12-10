89ab54d97780cd0bfab434ef930a1e084242f906
washosk <fernando@ethereumonarm.com>
2025-12-10 10:08:01 +0100

    feat: Add custom CSS and logo to the Sphinx documentation theme.

--- docs/conf.py
+++ docs/conf.py
@@ -79,6 +79,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_logo = '_static/images/eoa_logo.png'
+html_css_files = [
+    'css/custom.css',
+]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
